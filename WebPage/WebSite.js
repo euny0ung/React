c62c01d53df35ea063e91980107ebee68d43cfd1
euny0ung <56199780+euny0ung@users.noreply.github.com>
2022-03-10 22:20:24 +0900

    Add files via upload
    부트스트랩을 이용하여 네비게이션바를 가져오고 리액트 라우터를 이용하여 페이지 이동

---
+++ WebPage/WebSite.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class WebSite extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>site</h1>
+            </div>
+        );
+    }
+}
+
+export default WebSite;
